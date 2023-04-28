98a767f5bd144413c945dfa0949fb5dabb546f8d
Ripsou <48862603+dede13210@users.noreply.github.com>
2023-04-28 13:30:48 +0200

    interface graphique à faire

--- Batiment.java
+++ Batiment.java
@@ -5,8 +5,14 @@ public class Batiment {
     public Batiment(String nom){
         this.nom=nom;
     }
-    public void creerConcierge(Concierge concierge){
-        this.concierge=concierge;
+    public void creerConcierge(String nom){
+        Concierge concierge1=new Concierge(nom);
+        this.concierge=concierge1;
+    }
+    public void creerBavard(String nom){
+        Bavard bavard = new Bavard(nom);
+        bavard.ajouterConcierge(this.concierge);
+        this.concierge.ajouterBavard(bavard);
     }
 }
 
