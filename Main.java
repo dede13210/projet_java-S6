16e29400f178d42ed5d6a1b018052bb566ba2384
adam <dede13210@gmail.com>
2023-05-09 12:47:07 +0200

    CLEAN: main comment

--- Main.java
+++ Main.java
@@ -4,13 +4,18 @@ public class Main {
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
             try {
+                //on creer deux batiment, polytech et iae
                 Batiment polytech = new Batiment("polytech");
                 Batiment iae = new Batiment("iae");
+
+                //on creer un gestionnaire de batiment
                 GestionnaireBatiments gestionnaireBatiments = new GestionnaireBatiments();
-                // You can add Batiments to the GestionnaireBatiments here, if needed.
-                // For example: gestionnaireBatiments.ajouterBatiment(new Batiment("Mon Batiment"));
+
+                //on ajoute les deux batiments
                 gestionnaireBatiments.ajouterBatiment(polytech);
                 gestionnaireBatiments.ajouterBatiment(iae);
+
+                //on creer et on rend visible un fenètre batimentGUI
                 BatimentGUI window = new BatimentGUI(gestionnaireBatiments);
                 window.frame.setVisible(true);
             } catch (Exception e) {
