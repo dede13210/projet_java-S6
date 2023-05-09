c80c57aabe789d0ab5f03b02acbf04e7975a753f
adam <dede13210@gmail.com>
2023-05-09 13:52:57 +0200

    CLEAN: OnLineBavardEvent comment

--- OnLineBavardEvent.java
+++ OnLineBavardEvent.java
@@ -4,15 +4,18 @@ public class OnLineBavardEvent extends EventObject {
     private String connectMessage = "vient de se connecter";
     private String bavard ;
 
+    //constructeur prend en paramètre la source de l'event sous forme d'object et de string
     public OnLineBavardEvent(Object source,String bavard) {
         super(source);
         this.bavard = bavard;
     }
 
+    //renvoie l'attribut bavard
     public String getBavard() {
         return bavard;
     }
 
+    //renvoie l'event sous forme de string
     @Override
     public String toString() {
         return bavard +' '+ connectMessage;
