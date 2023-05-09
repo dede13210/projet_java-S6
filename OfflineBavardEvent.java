aca4f8b4595f9f7fa71d363c87f280511f7567c8
Arthur <fantache02@gmail.com>
2023-05-09 13:55:26 +0200

    CLEAN : commentaire dans OfflineBavardEvent

--- OfflineBavardEvent.java
+++ OfflineBavardEvent.java
@@ -4,15 +4,17 @@ public class OfflineBavardEvent extends EventObject {
     private String connectMessage = "vient de se déconnecter";
     private String bavard ;
 
+    //Créer un évènement de déconnexion
     public OfflineBavardEvent(Object source,String bavard) {
         super(source);
         this.bavard = bavard;
     }
 
+    //renvoie le nom du bavard qui se déconnecte
     public String getBavard() {
         return bavard;
     }
-
+    //permet d'afficher le message de déconnexion
     @Override
     public String toString() {
         return bavard +' '+ connectMessage;
