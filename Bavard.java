567ee8758bcf2015f265a94cdf866d9376f73faa
adam <dede13210@gmail.com>
2023-05-09 13:06:36 +0200

    CLEAN: bavard comment

--- Bavard.java
+++ Bavard.java
@@ -6,14 +6,18 @@ public class Bavard implements IPapotageListener {
     private BavardGUI bavardGUI;
     private ArrayList<Bavard> listBavardConnecte;
 
+    //constructeur
     public Bavard(String nom) {
         this.nom = nom;
         this.listConcierge=new ArrayList<Concierge>();
     }
+
+    //fonction qui lie une fenetre bavard gui au bavard
     public void addBavardGUI(BavardGUI bavardGUI){
         this.bavardGUI = bavardGUI;
     }
 
+    //les getters
     public String getNom() {
         return nom;
     }
@@ -22,12 +26,13 @@ public class Bavard implements IPapotageListener {
         return listConcierge;
     }
 
+    //ajoute un concierge à la liste de concierge
     public void ajouterConcierge(Concierge concierge) {
         this.listConcierge.add(concierge);
     }
 
 
-
+    // donne les attribut sous forme de string
     @Override
     public String toString() {
         return "Bavard{" +
@@ -35,13 +40,14 @@ public class Bavard implements IPapotageListener {
                 '}';
     }
 
-
+    //affiche le nouveau message reçu
     @Override
     public void newMessageRecu(PapotageEvent message) {
         bavardGUI.messageListener(message);
 
     }
 
+    //creer un papotage en prennant comme argument un sujet et un corps
     @Override
     public void createPapotage(String sujet, String corps) {
         PapotageEvent papotage=new PapotageEvent(this,this.nom,sujet,corps);
@@ -51,11 +57,13 @@ public class Bavard implements IPapotageListener {
 
     }
 
+    //affiche un nouvelle utilisateur connecte
     @Override
     public void newUserConnected(OnLineBavardEvent connect) {
         bavardGUI.connectListener(connect);
 
     }
+    //affiche un nouvelle utilisateur deconnecte
     @Override
     public void newUserDisconnected(OfflineBavardEvent disconnect) {
         bavardGUI.disconnectListener(disconnect);
