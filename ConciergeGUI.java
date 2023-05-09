9b25333e1b24faf76ba9680e4e4820faee441a36
Arthur <fantache02@gmail.com>
2023-05-09 13:47:09 +0200

    CLEAN : commentaire dans ConciergeGUI

--- ConciergeGUI.java
+++ ConciergeGUI.java
@@ -7,18 +7,20 @@ public class ConciergeGUI {
     private DefaultListModel<String> listModel;
 
     private Concierge concierge;
-
+    //création de ConciergeGUI
     public ConciergeGUI(Concierge concierge) {
         this.concierge = concierge;
         initialize();
     }
 
     private void initialize() {
+        //création de la fenêtre principale
         frame = new JFrame("Concierge - " + concierge.getNom());
         frame.setBounds(100, 100, 450, 300);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.getContentPane().setLayout(new BorderLayout(0, 0));
 
+        //implémentation de la liste des messages
         listModel = new DefaultListModel<>();
         listMessages = new JList<>(listModel);
         listMessages.setBackground(Color.pink);
@@ -29,7 +31,7 @@ public class ConciergeGUI {
     public void show() {
         frame.setVisible(true);
     }
-
+    //Permet d'afficher un messsage reçu dans l'interface
     public void addMessage(PapotageEvent message) {
         listModel.addElement(message.getNomBavard().toString()+'-'+message.getSujet() + " - " + message.getCorps()+ "...");
     }
