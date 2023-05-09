0121aee0bc52b9c9d08c5e19c8435b67c8423a75
adam <dede13210@gmail.com>
2023-05-09 13:43:39 +0200

    CLEAN: GestionnaireBatiment comment

--- GestionnaireBatiments.java
+++ GestionnaireBatiments.java
@@ -3,14 +3,17 @@ import java.util.ArrayList;
 public class GestionnaireBatiments {
     private ArrayList<Batiment> batiments;
 
+    //constructeur prend en parametre une liste de batiments
     public GestionnaireBatiments() {
         batiments = new ArrayList<>();
     }
 
+    //ajoute un batiment à la liste de batyiment
     public void ajouterBatiment(Batiment batiment) {
         batiments.add(batiment);
     }
 
+    //retrouve un batiment par son nom
     public Batiment getBatimentByName(String nom) {
         for (Batiment batiment : batiments) {
             if (batiment.getNom().equals(nom)) {
@@ -19,7 +22,7 @@ public class GestionnaireBatiments {
         }
         return null;
     }
-
+    //getter de batiments: retourne un liste de batiment
     public ArrayList<Batiment> getBatiments() {
         return batiments;
     }
